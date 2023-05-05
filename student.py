ee79a084b88228b161b00a1f38a63b597b2a6787
ajuno03 <132403986+ajuno03@users.noreply.github.com>
2023-05-05 10:02:42 -0500

    adding funtions
    Get name function that returns student name
    And get score that returns student score
    a set_name and set_score function that returns nothing
    Init function only takes in self
    Get grade function

---
+++ student.py
@@ -0,0 +1,28 @@
+class Student:
+    def __init__(self):
+        self.name = ""
+        self.score = 0
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def set_score(self, score):
+        self.score = score
+
+    def get_score(self):
+        return self.score
+
+    def get_grade(self):
+        if self.score >= 90:
+            return "A"
+        elif self.score >= 80:
+            return "B"
+        elif self.score >= 70:
+            return "C"
+        elif self.score >= 60:
+            return "D"
+        else:
+            return "F"
