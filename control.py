8c66b174901ba1503ef358b468c915fb518e9ef2
Caeden Webb <caedenwebb@unomaha.edu>
2023-05-09 23:59:16 -0500

    GUI Complete; calculate_grades() function removed; add and remove student functions added

--- control.py
+++ control.py
@@ -1,36 +1,63 @@
 from student import Student
 
 students = [] #creates a list to store student objects
-scores = [] #creates a list to store students scores
-def calculate_grades(window):
 
-    #gets Student Names and Scores from User; places Student Names and Scores in a list
-    StudentDataText = window.StudentData.toPlainText().split('\n')
-    #loop through the StudentDataText list and splits the string in each index position on ','
-    i = 0
-    while i < len(StudentDataText):
-        StudentDataText[i] = StudentDataText[i].split(',')
-        i = i + 1
-    #fills students list with student objects for each Student Name and Score
-    for ind_student in StudentDataText:
-        s = Student(ind_student[0], int(ind_student[1]))
-        students.append(s)
-
-    #retrieves scores and places them in scores list
-    for ind_student in students:
-        scores.append(ind_student.get_score())
-    #calculates best score
+def add_student(window):
+    scores = []  # creates a list to store students scores
+    '''
+    Adds a Student
+    :param window: Takes in the MainWindow
+    :return: None
+    '''
+    s = Student(window.nameInput.text(), float(window.scoreInput.text()))
+    students.append(s)
+    print(f'Students: {students}')
+    for student in students:
+        scores.append(student.get_score())
+    print(f'Scores: {scores}')
     best = max(scores)
-    for ind_student in students:
-        if (ind_student.get_score() >= (best - 10)):
-            ind_student.set_grade('A')
-        elif (ind_student.get_score() >= (best - 20)):
-            ind_student.set_grade('B')
-        elif (ind_student.get_score() >= (best - 30)):
-            ind_student.set_grade('C')
-        elif (ind_student.get_score() >= (best - 40)):
-            ind_student.set_grade('D')
+    print(f'Best: {best}')
+    for student in students:
+        if (student.get_score() >= (best - 10)):
+            student.set_grade('A')
+            continue
+        elif (student.get_score() >= (best - 20)):
+            student.set_grade('B')
+        elif (student.get_score() >= (best - 30)):
+            student.set_grade('C')
+        elif (student.get_score() >= (best - 40)):
+            student.set_grade('D')
         else:
-            ind_student.set_grade('F')
+            student.set_grade('F')
+    window.studentNamesList.setText('')
+    window.studentScoresList.setText('')
+    window.studentGradesList.setText('')
+    for student in students:
+        window.studentNamesList.setText(window.studentNamesList.toPlainText() + f'{student.get_name()}\n')
+        window.studentScoresList.setText(window.studentScoresList.toPlainText() + f'{student.get_score()}\n')
+        window.studentGradesList.setText(window.studentGradesList.toPlainText() + f'{student.get_grade()}\n')
+def delete_student(window):
+    '''
+    Deletes a student
+    :param window: takes in the MainWindow
+    :return: None
+    '''
+    i = 0
+    studentFound = False
+    for student in students:
+        if (student.get_name() == window.nameInput.text()):
+            studentFound = True
+            students.pop(i)
+            break
+        else:
+            i = i + 1
+            continue
+    if (studentFound == True):
+        pass
+    else:
+        window.errorLabel.setText(f'Error: Student "{window.nameInput.text()}" not found.')
+    for i in range(len(students)):
+        window.studentNamesList.setText(window.studentNamesList.text() + '\n' + students[i].get_name())
+        window.studentScoresList.setText(window.studentScoresList.text() + '\n' + str(students[i].get_score()))
 
 
